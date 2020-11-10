2b34d84d4cb3725a553c5d83c642a7f2a0adc1de
ryanghayour <56457626+ryanghayour@users.noreply.github.com>
2020-11-10 14:47:35 -0500

    Create home.js

---
+++ psc-client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class home extends Component {
+    render() {
+        return (
+            <div>
+                Home page
+            </div>
+        )
+    }
+}
+
+export default home
