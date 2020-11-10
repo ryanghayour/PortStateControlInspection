d60c8f11e4a63c90a41591fe7b11d3684251d4f7
ryanghayour <56457626+ryanghayour@users.noreply.github.com>
2020-11-10 11:10:26 -0500

    Create config.js

---
+++ psc-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyAYIZKVogpOjKCcUfFxjkQM4zgrHTVNms8",
+    authDomain: "psc-inspection.firebaseapp.com",
+    databaseURL: "https://psc-inspection.firebaseio.com",
+    projectId: "psc-inspection",
+    storageBucket: "psc-inspection.appspot.com",
+    messagingSenderId: "437233149227",
+    appId: "1:437233149227:web:2f607c5c430ee88c81116e",
+    measurementId: "G-M5MGLRB25J"
+};
